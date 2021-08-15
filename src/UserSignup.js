0a435969c30d8723e3fec44d2cd435494e805dd1
izaiasdb <izaias.dantas@gmail.com>
2021-08-15 19:57:48 -0300

    primeiro commit

--- src/UserSignup.js
+++ src/UserSignup.js
@@ -2,24 +2,40 @@ import React, { useState } from 'react';
 
 import './UserSignup.css';
 
+const initialState = {
+  userName: '',
+  email: '',
+  password: '',
+  passwordConfirmation: '',
+  investmentInterest: false
+};
+
 const UserSignup = () => {
-  const [userName, setUserName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [passwordConfirmation, setPasswordConfirmation] = useState('');
-  const [investmentInterest, setInvestmentInterest] = useState(false);
+  // const [userName, setUserName] = useState('');
+  // const [email, setEmail] = useState('');
+  // const [password, setPassword] = useState('');
+  // const [passwordConfirmation, setPasswordConfirmation] = useState('');
+  // const [investmentInterest, setInvestmentInterest] = useState(false);
+  const [state, setState] = useState(initialState)
 
   const handleSubmit = event => {
     event.preventDefault();
     clear();
   };
 
+  const handleChange = event => {
+    setState({
+      [event.target.name]: event.target.value
+    });
+  };
+
   const clear = () => {
-    setUserName('');
-    setEmail('');
-    setPassword('');
-    setPasswordConfirmation('');
-    setInvestmentInterest(false);
+    setState(initialState)
+    // setUserName('');
+    // setEmail('');
+    // setPassword('');
+    // setPasswordConfirmation('');
+    // setInvestmentInterest(false);
   };
 
   return (
@@ -29,52 +45,64 @@ const UserSignup = () => {
         id="userName"
         name="userName"
         type="text"
-        value={userName}
+        //value={userName}
+        value={state.userName}
         required
-        onChange={event => {
-          setUserName(event.target.value);
-        }}
+        // onChange={event => {
+        //   setUserName(event.target.value);
+        // }}
+        onChange={handleChange}        
       />
       <label htmlFor="email">Email Address</label>
       <input
         id="email"
         name="email"
         type="email"
-        value={email}
+        // value={email}
+        value={state.email}
         required
-        onChange={event => {
-          setEmail(event.target.value);
-        }}
+        // onChange={event => {
+        //   setEmail(event.target.value);
+        // }}
+        onChange={handleChange}
       />
       <label htmlFor="password">Password</label>
       <input
         id="password"
         name="password"
         type="password"
-        value={password}
+        // value={password}
+        value={state.password}        
         required
-        onChange={event => {
-          setPassword(event.target.value);
-        }}
+        // onChange={event => {
+        //   setPassword(event.target.value);
+        // }}
+        onChange={handleChange}
       />
       <label htmlFor="passwordConfirmation">Confirm Password</label>
       <input
         id="passwordConfirmation"
         name="passwordConfirmation"
         type="password"
-        value={passwordConfirmation}
+        // value={passwordConfirmation}
+        value={state.passwordConfirmation}
         required
-        onChange={event => {
-          setPasswordConfirmation(event.target.value);
-        }}
+        // onChange={event => {
+        //   setPasswordConfirmation(event.target.value);
+        // }}
+        onChange={handleChange}
       />
       <label htmlFor="investmentInterest" className="UserSignup--checkbox">
         <input
           id="investmentInterest"
           name="investmentInterest"
           type="checkbox"
-          checked={investmentInterest}
-          onChange={event => setInvestmentInterest(event.target.checked)}
+          //checked={investmentInterest}
+          checked={state.investmentInterest}
+          //onChange={event => setInvestmentInterest(event.target.checked)}
+          onChange={event => 
+            setState({investmentInterest : !event.target.checked})
+          }
         />
         Do you want to maybe help us out with an angel investment?
       </label>
